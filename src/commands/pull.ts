73c31fc2d7e66cd055d8831d8013239e68304350
pascal <pascal.morin@codeenigma.com>
2020-05-13 10:47:12 +0200

    Fix push command

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -1,32 +1,12 @@
 import {execSync} from 'child_process'
 
 import DockerImagesCmd from '../base-cmd-abstract-docker-images'
-import CeDevConfig from '../ce-dev-config-interface'
 import ComposeConfigService from '../compose-config-service-interface'
 export default class PullCmd extends DockerImagesCmd {
   static description = 'Pull images referenced in a compose file from a remote repository.'
   static examples = [
     '$ ce-dev pull --template example.compose.yml',
   ]
-
-  /**
-   * @inheritdoc
-   */
-  public constructor(argv: string[], config: any) {
-    super(argv, config)
-    const {flags} = this.parse(DockerImagesCmd)
-    this.composeTemplate = this.getPathFromRelative(flags.template)
-    this.composeConfig = this.LoadComposeConfig(this.composeTemplate) as CeDevConfig
-    if (flags.username) {
-      this.dockerUsername = flags.username
-    }
-    if (flags.password) {
-      this.dockerPassword = flags.password
-    }
-    if (flags.anonymous) {
-      this.dockerLogin = false
-    }
-  }
 
   /**
    * @inheritdoc
--- src/commands/push.ts
+++ src/commands/push.ts
@@ -1,30 +1,11 @@
 import {execSync} from 'child_process'
 
 import DockerImagesCmd from '../base-cmd-abstract-docker-images'
-import CeDevConfig from '../ce-dev-config-interface'
 export default class PushCmd extends DockerImagesCmd {
   static description = 'Push images referenced in a compose file to a remote repository.'
   static examples = [
     '$ ce-dev push --template example.compose.yml',
   ]
-  /**
-   * @inheritdoc
-   */
-  public constructor(argv: string[], config: any) {
-    super(argv, config)
-    const {flags} = this.parse(PushCmd)
-    this.composeTemplate = this.getPathFromRelative(flags.template)
-    this.composeConfig = this.LoadComposeConfig(this.composeTemplate) as CeDevConfig
-    if (flags.username) {
-      this.dockerUsername = flags.username
-    }
-    if (flags.password) {
-      this.dockerPassword = flags.password
-    }
-    if (flags.anonymous) {
-      this.dockerLogin = false
-    }
-  }
 
   /**
    * @inheritdoc
