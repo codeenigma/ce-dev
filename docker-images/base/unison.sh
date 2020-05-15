a009c4323fc7375ce7342f33284699eb5c19a4d6
pascal <pascal.morin@codeenigma.com>
2020-05-15 19:07:52 +0200

    Remove unison ignore and add threshold for large files

--- docker-images/base/unison.sh
+++ docker-images/base/unison.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 while true; do
-  flock /tmp/unison.lock /usr/local/bin/unison -owner -group -batch -ignore "Name vendor" -ignore "Name node_modules" -ignore "Path */sites/*/files" -repeat watch -fastercheckUNSAFE -prefer /.x-ce-dev /.x-ce-dev /home/ce-dev/deploy/live.local
+  flock /tmp/unison.lock /usr/local/bin/unison -copythreshold 100000 -owner -group -batch -repeat watch -fastercheckUNSAFE -prefer /.x-ce-dev /.x-ce-dev /home/ce-dev/deploy/live.local
 done
