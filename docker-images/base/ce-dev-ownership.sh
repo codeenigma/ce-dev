a61d77df4691dc655776563b4fc076546294d3c7
pascal <pascal.morin@codeenigma.com>
2020-07-22 11:39:18 +0200

    Fix permissions script

--- docker-images/base/ce-dev-ownership.sh
+++ docker-images/base/ce-dev-ownership.sh
@@ -10,20 +10,20 @@ ensure_user_ids(){
   OLD_GID="$(id -g ce-dev)"
   if [ "$OLD_UID" != "$1" ]; then
     usermod -u "$1" ce-dev
-    chown -R --from="$OLD_UID" "$NEW_UID" /var
+    chown -R --from="$OLD_UID" "$1" /var
     if [ -d /.x-ce-dev ]; then
-      chown -R "$NEW_UID" /.x-ce-dev
+      chown -R "$1" /.x-ce-dev
     fi
-    chown -R "$NEW_UID" /home/ce-dev
+    chown -R "$1" /home/ce-dev
     echo "User ID changed to $1."
   fi
   if [ "$OLD_GID" != "$2" ]; then
     groupmod -g "$2" ce-dev
-    chown -R --from=":$OLD_GID" ":$NEW_GID" /var
+    chown -R --from=":$OLD_GID" ":$2" /var
     if [ -d /.x-ce-dev ]; then
-      chown -R ":$NEW_GID" /.x-ce-dev
+      chown -R ":$2" /.x-ce-dev
     fi
-    chown -R ":$NEW_GID" /home/ce-dev
+    chown -R ":$2" /home/ce-dev
     echo "Group ID changed to $2."
   fi
 }
