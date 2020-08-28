0a58f62c69c6c392a241d304f3d0daad1a6a4f30
pascal <pascal.morin@codeenigma.com>
2020-08-28 13:30:38 +0200

    Fix ownership issue caused by mkcert mount

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -110,6 +110,7 @@ export default class StartCmd extends BaseCmd {
     }
     ux.action.start('Ensuring file ownership')
     execSync(this.dockerBin + ' exec ' + containerName + ' /bin/sh /opt/ce-dev-ownership.sh ' + uid.toString() + ' ' + gid.toString(), {stdio: 'inherit'})
+    execSync(this.dockerBin + ' exec ' + containerName + ' chown -R ce-dev:ce-dev /home/ce-dev/.local', {stdio: 'inherit'})
     ux.action.stop()
   }
 
