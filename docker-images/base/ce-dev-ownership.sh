05899aa92266ac08734a7affa6fe24b27f032091
Dionisio Fernandez <dionisiofernandez83@gmail.com>
2023-03-03 16:38:23 +0100

    64695: Removed verbosity

--- docker-images/base/ce-dev-ownership.sh
+++ docker-images/base/ce-dev-ownership.sh
@@ -12,25 +12,19 @@ ensure_user_ids(){
     return
   fi
   if [ "$OLD_UID" != "$1" ]; then
-    echo "Usermod $1."
-    sudo usermod -u "$1" ce-dev
-    echo "Chown $1."
-    sudo chown -R --from="$OLD_UID" "$1" /var
+    usermod -u "$1" ce-dev
+    chown -R --from="$OLD_UID" "$1" /var
     echo "User ID changed to $1."
   fi
   if [ "$OLD_GID" != "$2" ]; then
-    echo "Groupmod $2."
-    sudo groupmod -g "$2" ce-dev
-    echo "Chown $2."
-    sudo chown -R --from=":$OLD_GID" ":$2" /var
+    groupmod -g "$2" ce-dev
+    chown -R --from=":$OLD_GID" ":$2" /var
     echo "Group ID changed to $2."
   fi
   if [ -d /.x-ce-dev ]; then
-    echo "Chown ce-dev x"
-      sudo chown -R ce-dev:ce-dev /.x-ce-dev
+      chown -R ce-dev:ce-dev /.x-ce-dev
   fi
-  echo "Chown ce-dev home"
-  sudo chown -R ce-dev:ce-dev /home/ce-dev
+  chown -R ce-dev:ce-dev /home/ce-dev
 }
 
 # Match ids with host user.
