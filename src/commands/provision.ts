654a51fc8b7e580b0626e4b5c4597953c0ccef9a
gregharvey <greg.harvey@gmail.com>
2024-01-26 15:00:27 +0100

    Adding ability to switch ce-provision own branch and config branch.

--- src/commands/provision.ts
+++ src/commands/provision.ts
@@ -1,7 +1,26 @@
 import AnsibleCmd from '../base-cmd-ansible-abstract'
+import {flags} from '@oclif/command'
 
 export default class ProvisionCmd extends AnsibleCmd {
   static description = 'Provision containers with Ansible playbooks.'
+
+  static examples = [
+    '$ ce-dev provision --branch 1.x --config 1.x',
+  ]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    branch: flags.string({
+      char: 'b',
+      description: 'The branch of ce-provision to use for provisioning your containers. See https://github.com/codeenigma/ce-provision for options.',
+      default: '1.x',
+    }),
+    config: flags.string({
+      char: 'c',
+      description: 'The branch of the ce-provision-config repository. See https://github.com/codeenigma/ce-dev-ce-provision-config for options.',
+      default: '1.x',
+    }),
+  }
 
   protected ansibleProjectPlaybooksPath = '/home/ce-dev/projects-playbooks/provision'
 
@@ -9,21 +28,26 @@ export default class ProvisionCmd extends AnsibleCmd {
 
   protected ansibleScript = 'scripts/provision.sh'
 
+  protected ownBranch = '1.x'
+
+  protected configBranch = '1.x'
+
   /**
    * @inheritdoc
    */
   public constructor(argv: string[], config: any) {
     super(argv, config)
+    const {flags} = this.parse(ProvisionCmd)
     this.ansiblePaths = this.activeProjectInfo.provision
+    this.ownBranch = flags.branch
+    this.configBranch = flags.config
   }
 
   protected getCommandParameters(ansiblePath: string): string {
     const workspace = this.ansibleProjectPlaybooksPath
     const repo = this.activeProjectInfo.project_name
-    const ownBranch = '1.x'
-    const configBranch = '1.x'
-    let cmd = '--own-branch ' + ownBranch
-    cmd += ' --config-branch ' + configBranch
+    let cmd = '--own-branch ' + this.ownBranch
+    cmd += ' --config-branch ' + this.configBranch
     cmd += ' --workspace ' + workspace
     cmd += ' --repo ' + repo
     cmd += ' --branch ce-dev --playbook ' + ansiblePath
