d4c9a123b6199ba6088b6fa36da7ab7082df0c03
Dionisio Fernandez <dionisiofernandez83@gmail.com>
2024-03-08 13:12:48 +0100

    2.x: Removed hardcoded --debug

--- src/controller-manager.ts
+++ src/controller-manager.ts
@@ -95,7 +95,7 @@ export default class ControllerManager {
       this.dockerComposeBin +
       ' -f ' +
       this.controllerComposeFile +
-      ' -p ce_dev_controller up -d --debug',
+      ' -p ce_dev_controller up -d',
       {cwd: this.config.dataDir, stdio: 'inherit'},
     )
     // Ensure uid/gid.
