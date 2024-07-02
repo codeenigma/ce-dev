81623aea59c6b768010cdfe9d6ad9522f1074e09
Dionisio Fernandez <dionisiofernandez83@gmail.com>
2024-07-02 11:06:21 +0200

    2.x: Using tsx in dev mode

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
+#!/usr/bin/env tsx
 // eslint-disable-next-line node/shebang, unicorn/prefer-top-level-await
 async function main() {
   const {execute} = await import('@oclif/core')
